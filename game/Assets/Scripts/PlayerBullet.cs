bd1056f7f4b060cc3238b84851649523976dfb72
Patryk Wypych <64524899+c3rb3rr@users.noreply.github.com>
2023-01-02 18:48:55 +0100

    added gun that can fire a bullets
    TODO: when two bullet collide with each other they will disappear

---
+++ game/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float bulletSpeed = 7.5f;
+
+    public Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.velocity = transform.right * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
--- game/Assets/Scripts/PlayerController.cs
+++ game/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,68 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    private Vector2 moveInput;
-    
+    private Vector2 _moveInput;
+    public Rigidbody2D rb2d;
+    public Transform gunArm;
+    private Camera _cam;
+    public Animator anim;
+    // reference to the bullet that we want to fire
+    public GameObject bullet;
+    //from where we are fireing the bullet (position on the world)
+    public Transform fireStartPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
-        transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed,
-            moveInput.y * Time.deltaTime * moveSpeed, 0f);
+        _moveInput.x = Input.GetAxisRaw("Horizontal");
+        _moveInput.y = Input.GetAxisRaw("Vertical");
+        _moveInput.Normalize();
+        
+        // transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed,
+        //     moveInput.y * Time.deltaTime * moveSpeed, 0f);
+        rb2d.velocity = _moveInput * moveSpeed;
+        // position of mouse
+        Vector3 mousePos = Input.mousePosition;
+        // position of main camera
+        Vector3 screenPoint = _cam.WorldToScreenPoint(transform.localPosition);
+        
+        //rotating player
+        if (mousePos.x < screenPoint.x) // to the left of the player
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            gunArm.localScale = new Vector3(1f, 1f, 1f);
+        }
 
+        // ratate gun
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        // angle that gun should point to
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+        if (Input.GetMouseButtonDown(0)) // if lpm  is clicked
+        {
+            // create a copy of specific object
+            Instantiate(bullet, fireStartPoint.position, fireStartPoint.rotation);
+        }
+        
+        // animation of walking
+        if (_moveInput != Vector2.zero)
+        {
+            anim.SetBool("isPlayerMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isPlayerMoving", false);
+        }
     }
 }
